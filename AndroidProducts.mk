b7e60bd534a60b251c890f7593cf9d53c738bbc5
Shivamingale3 <shivamingale3@gmail.com>
2024-03-19 19:05:19 +0530

    matrixx to lineage

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2023 The AwakenOS Project
+# Copyright (C) 2023 The Project-Matrixx
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/matrixx_tundra.mk
+    $(LOCAL_DIR)/lineage_tundra.mk
 
 COMMON_LUNCH_CHOICES := \
-    matrixx_tundra-user \
-    matrixx_tundra-userdebug \
-    matrixx_tundra-eng
+    lineage_tundra-user \
+    lineage_tundra-userdebug \
+    lineage_tundra-eng
